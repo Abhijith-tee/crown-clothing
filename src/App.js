fd95217b896e0006ee6c43faa74d48b230b6285e
Abhijith T <abhijiththayad111@gmail.com>
2020-08-23 12:14:55 +0530

    moving Google signIn into sagas

--- src/App.js
+++ src/App.js
@@ -26,22 +26,22 @@ class App extends React.Component {
 
     const { setCurrentUser } = this.props;
 
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
+    // this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    //   if (userAuth) {
+    //     const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapShot => {
-            setCurrentUser({
-              id: snapShot.id,
-              ...snapShot.data()
-            });
-        });
-      } 
-      else {
-        setCurrentUser(userAuth);
-      }
+    //     userRef.onSnapshot(snapShot => {
+    //         setCurrentUser({
+    //           id: snapShot.id,
+    //           ...snapShot.data()
+    //         });
+    //     });
+    //   } 
+    //   else {
+    //     setCurrentUser(userAuth);
+    //   }
 
-    });
+    // });
   }
 
   componentWillUnmount() {
