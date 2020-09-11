75a8387290803de743ed26e494f43204601c7f13
Abhijith T <abhijiththayad111@gmail.com>
2020-09-11 12:31:04 +0530

    useEffect in App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -16,20 +16,12 @@ import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
 
 
-class App extends React.Component {
+const App = ({checkUserSession, currentUser}) => {
   
-  unsubscribeFromAuth = null
-
-  componentDidMount() {
-    const { checkUserSession } = this.props;
+  useEffect(() => {
     checkUserSession();
-  }
-
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
-
-  render() {
+  }, [checkUserSession]);
+  
     return (
       <div>
         <Header />
@@ -38,14 +30,11 @@ class App extends React.Component {
           <Route path='/shop' component={ShopPage} />
           <Route path='/checkout' component={CheckoutPage} />
           <Route exact path='/signin' render={() => 
-           this.props.currentUser 
-           ? (<Redirect to='/' />)
-           : (<SignInAndSignUpPage />)} />
+           currentUser ? (<Redirect to='/' />): (<SignInAndSignUpPage />)} />
         </Switch>
       </div>
     );
   }
-}
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
